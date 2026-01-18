46e90711c5dc50be42dc018673d8f3d119ce8db7
kdh4win4 <kdh4win4@users.noreply.github.com>
2026-01-18 15:28:23 -0500

    Create constants.py

---
+++ src/biollm/constants.py
@@ -0,0 +1,7 @@
+"""
+Project-wide constants and small utilities.
+"""
+
+SEP = " [SEP] "  # Explicit separator for prompt-like inputs
+DEFAULT_BASE_MODEL = "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract"
+DEFAULT_MAX_LENGTH = 256
